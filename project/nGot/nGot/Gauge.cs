050c8c22130a260459da9a2f4bb8bd0e89e3fd60
adamhoge <adhoge@gmail.com>
2013-09-16 19:32:30 -0400

    Create the Clamp and Gauge classes

---
+++ project/nGot/nGot/Gauge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace nGot
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class Gauge<T> where T : IComparable<T>
+    {
+        private T m_value;
+        private T m_min;
+        private T m_max;
+
+        public Gauge(T value, T min, T max)
+        {
+            m_value = value;
+            m_min = min;
+            m_max = max;
+
+            nGot.Math.Clamp<T>(m_value, m_min, m_max);
+        }
+    }
+}
---
+++ project/nGot/nGot/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace nGot
+{
+    class Math
+    {
+        /// <summary>
+        /// Clamp returns a number which is compared to a minimum and maximum allowable value.
+        /// </summary>
+        /// <typeparam name="T">A comparable value.</typeparam>
+        /// <param name="value">The value to compare.</param>
+        /// <param name="min">The minimum allowable result.</param>
+        /// <param name="max">The maximum allowable result.</param>
+        /// <returns></returns>
+        public static T Clamp<T>(T value, T min, T max) where T : IComparable<T>
+        {
+            T result;
+
+            // If the value is above or below the allowable values, constrain it to the closest allowable value.
+            result = value.CompareTo(min) < 0 ? min : value;
+            result = result.CompareTo(max) > 0 ? max : result;
+
+            return result;
+        }
+    }
+}
