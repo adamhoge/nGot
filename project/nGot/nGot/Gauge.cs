2f97e86184ad0f653231effaa22b13f639e4ef04
adamhoge <adhoge@gmail.com>
2014-04-06 15:00:59 -0400

    Updated the Gauge class

--- project/nGot/nGot/Gauge.cs
+++ project/nGot/nGot/Gauge.cs
@@ -8,9 +8,15 @@ namespace nGot
     /// <typeparam name="T"></typeparam>
     class Gauge<T> where T : IComparable<T>
     {
+        #region Member Variables
+        
         private T m_value;
         private T m_min;
         private T m_max;
+        
+        #endregion
+
+        #region Initialization
 
         public Gauge(T value, T min, T max)
         {
@@ -20,5 +26,21 @@ namespace nGot
 
             nGot.Math.Clamp<T>(m_value, m_min, m_max);
         }
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Accessors
+
+        public T Min 
+        { 
+            get { return m_min; }
+            set { m_min = value;  m_value = Math.Clamp<T>(m_value, m_min, m_max); }
+        }
+
+        #endregion
     }
 }
